6d9f8b11c6683125fe2fd411ca7d85ce7e09c089
Sajaki <Sajaki9@gmail.com>
2016-07-04 00:30:01 +0200

    add demon hunter

--- migrations/profile_fields_3_0_0.php
+++ migrations/profile_fields_3_0_0.php
@@ -639,7 +639,7 @@ class profile_fields_3_0_0 extends \phpbb\db\migration\migration
 				$this->db_tools->perform_schema_changes($schema_change);
 
 				// Copy the user data to the new column
-// TODO use buffer
+				// TODO use buffer
 				$sql = 'UPDATE ' . $this->table_prefix . 'profile_fields_data
 					SET pf_' . $profilefield_name . ' = pf_' . $meta['profilefield_oldname'];
 				$this->db->sql_query($sql);
